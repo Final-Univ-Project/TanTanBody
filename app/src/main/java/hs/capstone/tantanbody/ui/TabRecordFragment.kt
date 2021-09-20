506ae875032209a6d5841d90778184ab56e54bf9
HyunAh-Kim-Clou <wgrape0@gmail.com>
2021-09-21 01:39:12 +0900

    기록화면 운동목표 추가

--- app/src/main/java/hs/capstone/tantanbody/ui/TabRecordFragment.kt
+++ app/src/main/java/hs/capstone/tantanbody/ui/TabRecordFragment.kt
@@ -1,6 +1,10 @@
 package hs.capstone.tantanbody.ui
 
 import android.annotation.SuppressLint
+import android.app.AlertDialog
+import android.app.Dialog
+import android.content.DialogInterface
+import android.content.Intent
 import android.graphics.Color
 import android.os.Bundle
 import android.util.Log
@@ -8,11 +12,16 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.WindowManager
+import android.widget.EditText
+import android.widget.RelativeLayout
 import android.widget.TextView
+import androidx.core.view.marginTop
 import hs.capstone.tantanbody.R
 
 class TabRecordFragment : Fragment() {
     val TAG = "TabRecordFragment"
+    lateinit var goalTitleTv: TextView
     lateinit var fitnessGraphTitle: TextView
     lateinit var weightGraphTitle: TextView
 
@@ -25,9 +34,31 @@ class TabRecordFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         val layout = inflater.inflate(R.layout.fragment_tab_record, container, false)
+        goalTitleTv = layout.findViewById(R.id.goalTitleTv)
         fitnessGraphTitle = layout.findViewById(R.id.fitnessGraphTitle)
         weightGraphTitle = layout.findViewById(R.id.weightGraphTitle)
 
+        goalTitleTv.setOnClickListener {
+            val goalEt = EditText(this.context)
+            goalEt.hint = getString(R.string.goal_title)
+
+            val goalDlg: AlertDialog.Builder = AlertDialog.Builder(
+                this.context,
+                android.R.style.Theme_DeviceDefault_Light_Dialog_NoActionBar_MinWidth
+            )
+            goalDlg.setTitle(R.string.goal_dialog_title)
+            goalDlg.setView(goalEt)
+            goalDlg.setPositiveButton(
+                "확인",
+                DialogInterface.OnClickListener { dialog, which ->
+                    // ViewModel에 운동 목표 저장
+
+                    goalTitleTv.text = goalEt.text
+                }
+            )
+            goalDlg.show()
+            Log.d(TAG, "R.id.goalTitle 클릭")
+        }
         fitnessGraphTitle.setOnClickListener {
             loadGraphFragment(FitnessGraphFragment.newInstance())
             fitnessGraphTitle.setTextColor(getColorFrom(R.color.using_content))
